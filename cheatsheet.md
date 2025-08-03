0c8fbc5147e12b2debeb4c997ec4eca1ccb92936
sedataskan <sedan.taskan@gmail.com>
2025-08-03 21:41:26 +0300

    Add main program with conditional statements and map examples

--- cheatsheet.md
+++ cheatsheet.md
@@ -1,21 +1,27 @@
 # Cheatsheet for Go Development
 
 -> to run the Go program, use:
+
 ```bash
 go run main.go
 ```
 
 -> to build the mod file which contains dependencies, use:
+
 ```bash
 go mod init <module-name>
 ```
 
 -> to add a dependency, use:
+
 ```bash
 go get <package-name>
 ```
 
 -> to tidy up the module file and remove unused dependencies, use:
+
 ```bash
 go mod tidy
 ```
+
+- on Go, maps are unordered collections of key-value pairs. When you print a map, the order of the keys is not guaranteed to be the same every time you run the program.
---
+++ condiationals_for_maps/main.go
@@ -0,0 +1,89 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var age int = 25
+
+	if age < 18 {
+		fmt.Println("You are a minor.")
+	} else if age >= 18 && age < 65 {
+		fmt.Println("You are an adult.")
+	} else {
+		fmt.Println("You are a senior citizen.")
+	}
+	fmt.Println("****")
+
+	//for
+
+	index := 0
+
+	for index < 5 {
+		fmt.Println("Index:", index)
+		index++
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("For Loop Index:", i)
+	}
+
+	var arr = []string{"s", "n", "t"}
+
+	for index, value := range arr {
+		fmt.Println("Index:", index, "Value:", value)
+	}
+
+	for i := 1; i < 10; i++ {
+		if i%6 == 0 {
+			break
+		}
+		if i%2 == 0 {
+			continue // Skip even numbers
+		} else {
+			fmt.Println("Odd Index:", i)
+		}
+
+	}
+	fmt.Println("****")
+
+	//map
+
+	var m = map[string]int{"s": 1, "n": 2, "t": 3}
+	for key, value := range m {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+
+	fmt.Println("Value for key 's':", m["s"])
+	fmt.Println("Value for key 'ö':", m["ö"]) // This will return 0 since 'ö' is not a key in the map
+
+	delete(m, "s") // Deleting key 's' from the map
+	fmt.Println("Map after deletion:", m)
+	fmt.Println("****\n")
+
+	//for each loop with map
+	for key, value := range m {
+		if value > 1 {
+			fmt.Println("Key:", key, "Value:", value)
+		}
+	}
+
+	var lang string = "GoLang"
+
+	for _, char := range lang {
+		fmt.Println("Character:", char) // This will print the Unicode code point of the character
+		fmt.Printf("Character: %c\n", char)
+	}
+
+	fmt.Println("****")
+
+	var m1 = map[string]int{"s": 1, "n": 2, "t": 3}
+	for key, value := range m1 {
+		if value > 1 {
+			fmt.Println("Key:", key, "Value:", value)
+		} else {
+			fmt.Println("Skipping Key:", key, "Value:", value)
+			continue // Skip this iteration if value is not greater than 1
+		}
+	}
+}
